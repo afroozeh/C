89be579e781b1cec0b4b1b71dc3d607e70bc831c
Ali Afroozeh <afroozeh@gmail.com>
2015-09-06 19:08:42 +0200

    changes

--- src/org/iguana/OCaml.java
+++ src/org/iguana/OCaml.java
@@ -9,60 +9,54 @@ import org.iguana.grammar.Grammar;
 import org.iguana.grammar.precedence.OperatorPrecedence;
 import org.iguana.grammar.symbol.Nonterminal;
 import org.iguana.grammar.symbol.Start;
-import org.iguana.grammar.transformation.DesugarPrecedenceAndAssociativity;
 import org.iguana.grammar.transformation.EBNFToBNF;
 import org.iguana.grammar.transformation.LayoutWeaver;
-import org.iguana.parser.ParseResult;
 import org.iguana.util.IguanaRunner;
 import org.iguana.util.RunResult;
 import org.iguana.util.RunResultUtil;
 import org.iguana.util.SuccessResult;
 
-public class Java {
+public class OCaml {
 	
-	private static Grammar originalGrammar = Grammar.load(new File("grammar/JavaSpecificationCharLevel"));
+	private static Grammar originalGrammar = Grammar.load(new File("grammar/OCamlCharLevel"));
 //	private static Grammar grammar = new LayoutWeaver().transform(new DesugarPrecedenceAndAssociativity().transform(new EBNFToBNF().transform(originalGrammar)));
 //	private static Grammar grammar = new LayoutWeaver().transform(new EBNFToBNF().transform(originalGrammar));
 	private static Grammar grammar = new LayoutWeaver().transform(new OperatorPrecedence(originalGrammar.getPrecedencePatterns(), originalGrammar.getExceptPatterns()).transform(new EBNFToBNF().transform(originalGrammar)));
 	
 	private static Start start = grammar.getStartSymbol(Nonterminal.withName("CompilationUnit"));
 	
-	private static String slf4j = "/Users/aliafroozeh/corpus/Java/slf4j-1.7.12";
-	private static String junit = "/Users/aliafroozeh/corpus/Java/junit-r4.12";
-	private static String jdk1_7 = "/Users/aliafroozeh/corpus/Java/jdk1.7.0_60-b19";
+	private static String ocaml = "/Users/afroozeh/corpus/ocaml/ocaml";
 	
 	public static void main(String[] args) {
 				
 //		List<ParseResult> results = IguanaRunner.builder(grammar, Nonterminal.withName("StatementExpression")).addString("logger.info(\"Activator.start()\")").build().run();
 		
 //		System.out.println(originalGrammar.getNonterminals().size());
-//		List<RunResult> results = IguanaRunner.builder(grammar, start)
-//				                              .addFile("files/AMD64RawAssembler.java")
-//				                              .setWarmupCount(3)
-//				                              .setRunCount(5)
+		List<RunResult> results = IguanaRunner.builder(grammar, start)
+				                              .addFile("files/Test.ml")
+				                              .setWarmupCount(0)
+				                              .setRunCount(1)
 //				                              .setRunGCInBetween(true)
-//				                              .build().run();
+				                              .build()
+				                              .run();
 
 //		System.out.println(results);
 		
 //		System.out.println(grammar);
 		
-		List<RunResult> results = IguanaRunner.builder(grammar, start)
-				                              .setWarmupCount(3)
-				                              .setRunCount(5)
-//				                              .setRunGCInBetween(false)
-//				                              .setLimit(20)
-				                              .addDirectory(jdk1_7, "java", true)
-				                              .addDirectory(slf4j, "java", true)
-				                              .addDirectory(junit, "java", true)
-//				                              .addFile("files/Test.java")
-				                              .build()
-				                              .run();
+//		List<RunResult> results = IguanaRunner.builder(grammar, start)
+//				                              .setWarmupCount(0)
+//				                              .setRunCount(1)
+////				                              .setRunGCInBetween(false)
+////				                              .setLimit(10)
+//				                              .addDirectory(ocaml, "ml", true)
+////				                              .addFile("files/Test.java")
+//				                              .build()
+//				                              .run();
 		
-		System.out.println(results);
-
-		
-		System.out.println(RunResultUtil.format(results));
+//		System.out.println(results);
+//		
+//		System.out.println(RunResultUtil.format(results));
 		Map<URI, SuccessResult> groupByInput = RunResultUtil.groupByInput(results);
 		System.out.println(RunResultUtil.format(groupByInput));
 		System.out.println(RunResultUtil.summary(groupByInput));
